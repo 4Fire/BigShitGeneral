303d40c8eac0c127497c7462ea37db7de748301d
SingYi <23323231@qq.com>
2016-07-21 17:42:20 +0800

    Sing_yi reoladData

--- CoreData/Record/Record.h
+++ CoreData/Record/Record.h
@@ -2,48 +2,21 @@
 //  Record.h
 //  大将军
 //
-//  Created by apple on 16/7/14.
+//  Created by 石燚 on 16/7/21.
 //  Copyright © 2016年 SingYi. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class Dog;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Record : NSManagedObject
 
-+ (BOOL)insertRecordOfFamaleDogToSQLiterWithContext:(NSManagedObjectContext *)ctx
-                                                ppv:(NSDate *)ppv
-                                          distemper:(NSDate *)distemper
-                                        coronavirus:(NSDate *)coronavirus
-                                             rabies:(NSDate *)rabies
-                                         toxoplasma:(NSDate *)toxoplasma
-                                      ininsecticide:(NSDate *)ininsecticide
-                                     outinsecticide:(NSDate *)outinsecticide
-                                           pregnant:(NSDate *)pregnant
-                                           delivery:(NSDate *)delivery
-                                          neutering:(NSNumber *)neutering
-                                              other:(NSString *)other
-                                                Dog:(NSManagedObject *)dog;
+// Insert code here to declare functionality of your managed object subclass
 
-+ (BOOL)insertRecordOfMmaleDogToSQLiterWithContext:(NSManagedObjectContext *)ctx
-                                               ppv:(NSDate *)ppv
-                                         distemper:(NSDate *)distemper
-                                       coronavirus:(NSDate *)coronavirus
-                                            rabies:(NSDate *)rabies
-                                        toxoplasma:(NSDate *)toxoplasma
-                                     ininsecticide:(NSDate *)ininsecticide
-                                    outinsecticide:(NSDate *)outinsecticide
-                                         neutering:(NSNumber *)neutering
-                                             other:(NSString *)other
-                                               Dog:(NSManagedObject *)dog;
-
-+ (BOOL)duplicateCheckingRecordWithContext:(NSManagedObjectContext *)ctx
-                                   DogName:(NSString *)dogName;
-
-+ (Record *)fetchRecordToSQLiterWithContext:(NSManagedObjectContext *)ctx
-                                    DogName:(NSString *)dogName;
 @end
 
 NS_ASSUME_NONNULL_END
--- 大将军/ChildClass/2Record/Controller/RecordController.m
+++ 大将军/ChildClass/2Record/Controller/RecordController.m
@@ -9,19 +9,31 @@
 #import "RecordController.h"
 #import "TableRecord1Cell.h"
 #import "TableHeader.h"
+#import "Dog.h"
+#import "DoggyModel.h"
+#import "PersonDogsCell.h"
 
-@interface RecordController ()<UITableViewDelegate,UITableViewDataSource,TableHeaderDelegate>
+@interface RecordController ()<UITableViewDelegate,UITableViewDataSource,TableHeaderDelegate,UICollectionViewDelegate,UICollectionViewDataSource>
 
 //记录的table
 @property (nonatomic, strong) UITableView *tableView;
 //头部视图的日历
 @property (nonatomic, strong) TableHeader *tableHeader;
 
-@property (nonatomic, strong) NSArray *cellTitleArray;
+@property (nonatomic, strong) NSArray *cellTitleArrayOfMale;
+@property (nonatomic, strong) NSArray *cellTitleArrayOfFamale;
 
 //上一月和下一月
 @property (nonatomic, strong) UIButton *nextMonthBtn;
 @property (nonatomic, strong) UIButton *lastMonthBtn;
+
+//所有的狗
+@property (nonatomic, strong) NSArray<Dog *> *dogs;
+
+@property (nonatomic, strong) UICollectionView *collectionView;
+
+//gou
+@property (nonatomic, strong) Dog *dog;
 
 @end
 
@@ -34,6 +46,25 @@
         
     }
     return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.barTintColor = BACKGROUNDCOLOR;
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    
+//    UIImage *view = [self imageWithColor:BACKGROUNDCOLOR rect:self.navigationController.navigationBar.bounds];
+//    view = [self imageWithColor:BACKGROUNDCOLOR rect:self.navigationController.navigationBar.bounds];
+//    self.navigationController.navigationBar.backgroundColor = BACKGROUNDCOLOR;
+    
+//    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+//                                                 forBarPosition:UIBarPositionAny
+//                                                     barMetrics:UIBarMetricsDefault];
+//    
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    
+    [self.navigationController.navigationBar setShadowImage:[UIImage new]];
+    
 }
 
 - (void)viewDidLoad {
@@ -51,10 +82,17 @@
 
 //初始化界面
 - (void)initUserInterface {
-    self.view.backgroundColor = [UIColor blackColor];
     [self.view addSubview:self.tableView];
     [self.navigationController.navigationBar addSubview:self.nextMonthBtn];
     [self.navigationController.navigationBar addSubview:self.lastMonthBtn];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    formatter.dateFormat = @"YYYY年MM月d日";
+    self.navigationItem.title = [formatter stringFromDate:[NSDate date]];
+    
+    UIBarButtonItem *rigthBtn = [[UIBarButtonItem alloc]initWithTitle:@"今天" style:(UIBarButtonItemStyleDone) target:self action:@selector(clickToday)];
+    self.navigationItem.rightBarButtonItem = rigthBtn;
+
 }
 
 #pragma mark - talbeViewDataSource
@@ -63,25 +101,38 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.cellTitleArray.count;
+    if (self.dog.neutering) {
+        return self.cellTitleArrayOfFamale.count ;
+    } else {
+        return self.cellTitleArrayOfMale.count ;
+    }
+    
 }
+
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"tableCell" forIndexPath:indexPath];
-    cell.textLabel.text = self.cellTitleArray[indexPath.row];
-
-    UISwitch *cellSwitch = [[UISwitch alloc]init];
-    [cellSwitch addTarget:self action:@selector(switchAction:) forControlEvents:(UIControlEventValueChanged)];
-    cellSwitch.on = NO;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"tableCell" forIndexPath:indexPath];
     
-    cell.accessoryView = cellSwitch;
-    
-    if (indexPath.row == self.cellTitleArray.count - 1) {
-        cell.accessoryView = nil;
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;;
+    if (self.dog.neutering) {
+        cell.textLabel.text = self.cellTitleArrayOfFamale[indexPath.row];
+        
+        UISwitch *cellSwitch = [[UISwitch alloc]init];
+        [cellSwitch addTarget:self action:@selector(switchAction:) forControlEvents:(UIControlEventValueChanged)];
+        cellSwitch.on = NO;
+        
+        cell.accessoryView = cellSwitch;
+    } else {
+        cell.textLabel.text = self.cellTitleArrayOfMale[indexPath.row];
+        
+        UISwitch *cellSwitch = [[UISwitch alloc]init];
+        [cellSwitch addTarget:self action:@selector(switchAction:) forControlEvents:(UIControlEventValueChanged)];
+        cellSwitch.on = NO;
+        
+        cell.accessoryView = cellSwitch;
     }
-
+    
     return cell;
 }
 
@@ -95,13 +146,16 @@
 }
 
 #pragma mark - tableViewDelegate
+
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    return 10;
+    return SCREEN_HEIGHT * 0.13;
 }
 
-//- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-//    
-//}
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT * 0.3)];
+    view.backgroundColor = [UIColor redColor];
+    return self.collectionView;
+}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
     return 0.1;
@@ -117,6 +171,18 @@
     self.navigationItem.title = time;
 }
 
+- (void)TableHeader:(TableHeader *)tableHeader changeHeight:(BOOL)change {
+    if (change) {
+        self.tableHeader.bounds = CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_WIDTH / 7 * 6 + 20);
+    } else {
+        self.tableHeader.bounds = CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_WIDTH / 7 * 5 + 20);
+    }
+    [self.tableView beginUpdates];
+    self.tableView.tableHeaderView = self.tableHeader;
+    [self.tableView endUpdates];
+    
+}
+
 #pragma mark - nextMonthAndlastMonth
 - (void)nextMonth {
     [self.tableHeader nextMonth];
@@ -126,6 +192,9 @@
     [self.tableHeader lastMonth];
 }
 
+- (void)clickToday {
+    [self.tableHeader today];
+}
 
 #pragma mark - getter
 //table视图
@@ -142,7 +211,12 @@
         
         [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"tableCell"];
         
+        //隐藏滚动条
+        _tableView.showsVerticalScrollIndicator = NO;
+        
         _tableView.tableHeaderView = self.tableHeader;
+        
+        _tableView.bounces = NO;
     }
     return _tableView;
 }
@@ -152,8 +226,8 @@
     if (!_nextMonthBtn) {
         _nextMonthBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
         [_nextMonthBtn setTitle:@"下一月" forState:(UIControlStateNormal)];
-        _nextMonthBtn.frame = CGRectMake(SCREEN_WIDTH / 7 * 5, self.navigationController.navigationBar.bounds.size.height / 3, SCREEN_WIDTH / 7, self.navigationController.navigationBar.bounds.size.height / 2);
-        [_nextMonthBtn setBackgroundColor:[UIColor redColor]];
+        _nextMonthBtn.frame = CGRectMake(SCREEN_WIDTH / 16 * 11, self.navigationController.navigationBar.bounds.size.height / 3, SCREEN_WIDTH / 7, self.navigationController.navigationBar.bounds.size.height / 2);
+//        [_nextMonthBtn setBackgroundColor:[UIColor redColor]];
         _nextMonthBtn.titleLabel.font = [UIFont boldSystemFontOfSize:14];
         [_nextMonthBtn setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
         [_nextMonthBtn addTarget:self action:@selector(nextMonth) forControlEvents:(UIControlEventTouchUpInside)];
@@ -165,8 +239,8 @@
     if (!_lastMonthBtn) {
         _lastMonthBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
         [_lastMonthBtn setTitle:@"上一月" forState:(UIControlStateNormal)];
-        _lastMonthBtn.frame = CGRectMake(SCREEN_WIDTH / 7, self.navigationController.navigationBar.bounds.size.height / 3, SCREEN_WIDTH / 7, self.navigationController.navigationBar.bounds.size.height / 2);
-        [_lastMonthBtn setBackgroundColor:[UIColor redColor]];
+        _lastMonthBtn.frame = CGRectMake(SCREEN_WIDTH / 6, self.navigationController.navigationBar.bounds.size.height / 3, SCREEN_WIDTH / 7, self.navigationController.navigationBar.bounds.size.height / 2);
+//        [_lastMonthBtn setBackgroundColor:[UIColor redColor]];
         [_lastMonthBtn setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
         _lastMonthBtn.titleLabel.font = [UIFont boldSystemFontOfSize:14];
         [_lastMonthBtn addTarget:self action:@selector(lastMonth) forControlEvents:(UIControlEventTouchUpInside)];
@@ -178,20 +252,84 @@
 - (TableHeader *)tableHeader {
     if (!_tableHeader) {
         _tableHeader = [[TableHeader alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_WIDTH / 7 * 6 + 20)];
-        _tableHeader.backgroundColor = [UIColor whiteColor];
+        _tableHeader.backgroundColor = BACKGROUNDCOLOR;
         _tableHeader.tableHeaderDelegate = self;
     }
     
     return _tableHeader;
 }
 
-- (NSArray *)cellTitleArray {
-    if (!_cellTitleArray) {
-        _cellTitleArray = @[@"体外驱虫",@"体内驱虫",@"免疫疫苗",@"弓形虫免疫",@"怀孕",@"生宝宝",@"纪念日"];
+- (NSArray *)cellTitleArrayOfMale {
+    if (!_cellTitleArrayOfMale) {
+        _cellTitleArrayOfMale = @[@"体内驱虫",@"体外驱虫",@"细小病毒免疫",@"犬瘟热病毒免疫",@"冠状病毒免疫",@"狂犬病疫苗",@"弓形虫疫苗"];
     }
-    return _cellTitleArray;
+    return _cellTitleArrayOfMale;
 }
 
+- (NSArray *)cellTitleArrayOfFamale {
+    if (!_cellTitleArrayOfFamale) {
+        _cellTitleArrayOfFamale = @[@"体内驱虫",@"体外驱虫",@"细小病毒免疫",@"犬瘟热病毒免疫",@"冠状病毒免疫",@"狂犬病疫苗",@"弓形虫疫苗",@"怀孕",@"分娩"];
+    }
+    return _cellTitleArrayOfFamale;
+}
+
+- (NSArray<Dog *> *)dogs {
+    return [DoggyModel getAllDogsWithCurrentOwner];
+}
+
+- (UICollectionView *)collectionView {
+    if (!_collectionView) {
+        UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
+        layout.itemSize = CGSizeMake(SCREEN_WIDTH * 0.19, SCREEN_WIDTH * 0.19);
+        layout.minimumInteritemSpacing = SCREEN_WIDTH * 0.03;
+        layout.sectionInset = UIEdgeInsetsMake(SCREEN_WIDTH * 0.03, SCREEN_WIDTH * 0.03, SCREEN_WIDTH * 0.03, SCREEN_WIDTH * 0.03);
+        layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        
+        _collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT * 0.13) collectionViewLayout:layout];
+        _collectionView.backgroundColor = BACKGROUNDCOLOR;
+        _collectionView.showsHorizontalScrollIndicator = NO;
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        //注册原型cell
+        [_collectionView registerClass:[PersonDogsCell class] forCellWithReuseIdentifier:@"PersonDogsCelliii"];
+    }
+    return _collectionView;
+}
+
+#pragma mark - <UICollectionViewDelegate,UICollectionViewDataSource>
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    if (self.dogs.count < 3) {
+        self.collectionView.scrollEnabled = NO;
+    }else {
+        self.collectionView.scrollEnabled = YES;
+    }
+    return self.dogs.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    PersonDogsCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PersonDogsCelliii" forIndexPath:indexPath];
+    
+    cell.dogIcon.image = [UIImage imageWithData:self.dogs[indexPath.row].iconImage];
+
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    _dog = self.dogs[indexPath.item];
+}
+
+#pragma mark - other
+//imagewithColor
+- (UIImage *)imageWithColor:(UIColor *)color rect:(CGRect)rect {
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context,color.CGColor);
+    CGContextFillRect(context, rect);
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return img;
+}
 
 
 @end
